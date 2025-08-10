6b6275f69a5bca96d0cf13a68ba96b19bf66aa63
rlie0007 <rlie0007@student.monash.edu>
2025-08-10 18:16:11 +1000

    updated the code to show only one bay per zone/seg

--- sql/bay_segments.sql
+++ sql/bay_segments.sql
@@ -28,63 +28,70 @@ INNER JOIN parking_restrictions r   ON r.ParkingZone   = ss.ParkingZone;
 
 */
 
-
-WITH one_bay_per_segment AS (
-    SELECT pb.*
+WITH last_updated_bay AS (
+  SELECT sub.*
+  FROM (
+    SELECT
+      pb.*,
+      ROW_NUMBER() OVER (
+        PARTITION BY pb.RoadSegmentID
+        ORDER BY pb.LastUpdated DESC
+      ) AS rn
     FROM parking_bays pb
-    JOIN (
-        SELECT RoadSegmentID, MIN(KerbsideID) AS min_kerbside
-        FROM parking_bays
-        GROUP BY RoadSegmentID
-    ) x ON pb.RoadSegmentID = x.RoadSegmentID
-       AND pb.KerbsideID = x.min_kerbside
+  ) sub
+  WHERE rn = 1
 )
 SELECT 
-    ss.Segment_ID,
-    ss.ParkingZone,
-    ss.OnStreet,
-    ss.StreetFrom,
-    ss.StreetTo,
-    ob.RoadSegmentDescription,
-    ob.Latitude,
-    ob.Longitude,
-    r.Restriction_Days,
-    r.Time_Restrict_Start,
-    r.Time_Restrict_End,
-    r.Restriction_Display
+  ss.Segment_ID,
+  ss.ParkingZone,
+  ss.OnStreet,
+  ss.StreetFrom,
+  ss.StreetTo,
+  ob.RoadSegmentDescription,
+  ob.Latitude,
+  ob.Longitude,
+  ob.LastUpdated,
+  r.Restriction_Days,
+  r.Time_Restrict_Start,
+  r.Time_Restrict_End,
+  r.Restriction_Display
 FROM street_segments ss
-INNER JOIN one_bay_per_segment ob
-    ON ob.RoadSegmentID = ss.Segment_ID
+INNER JOIN last_updated_bay ob
+  ON ob.RoadSegmentID = ss.Segment_ID
 INNER JOIN parking_restrictions r
-    ON r.ParkingZone = ss.ParkingZone;
+  ON r.ParkingZone = ss.ParkingZone;
 
 # Create the segment bay table with restrictions
 CREATE TABLE bay_segments AS
-WITH one_bay_per_segment AS (
-    SELECT pb.*
+WITH last_updated_bay AS (
+  SELECT *
+  FROM (
+    SELECT
+      pb.*,
+      ROW_NUMBER() OVER (
+        PARTITION BY pb.RoadSegmentID
+        ORDER BY pb.LastUpdated DESC
+      ) AS rn
     FROM parking_bays pb
-    JOIN (
-        SELECT RoadSegmentID, MIN(KerbsideID) AS min_kerbside
-        FROM parking_bays
-        GROUP BY RoadSegmentID
-    ) x ON pb.RoadSegmentID = x.RoadSegmentID
-       AND pb.KerbsideID = x.min_kerbside
+  ) x
+  WHERE rn = 1
 )
 SELECT 
-    ss.Segment_ID,
-    ss.ParkingZone,
-    ss.OnStreet,
-    ss.StreetFrom,
-    ss.StreetTo,
-    ob.RoadSegmentDescription,
-    ob.Latitude,
-    ob.Longitude,
-    r.Restriction_Days,
-    r.Time_Restrict_Start,
-    r.Time_Restrict_End,
-    r.Restriction_Display
+  ss.Segment_ID,
+  ss.ParkingZone,
+  ss.OnStreet,
+  ss.StreetFrom,
+  ss.StreetTo,
+  lub.RoadSegmentDescription,
+  lub.Latitude,
+  lub.Longitude,
+  lub.LastUpdated,
+  r.Restriction_Days,
+  r.Time_Restrict_Start,
+  r.Time_Restrict_End,
+  r.Restriction_Display
 FROM street_segments ss
-INNER JOIN one_bay_per_segment ob
-    ON ob.RoadSegmentID = ss.Segment_ID
+INNER JOIN last_updated_bay lub
+  ON lub.RoadSegmentID = ss.Segment_ID
 INNER JOIN parking_restrictions r
-    ON r.ParkingZone = ss.ParkingZone;
+  ON r.ParkingZone = ss.ParkingZone;
