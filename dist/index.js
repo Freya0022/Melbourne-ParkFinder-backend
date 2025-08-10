9047822bd94e7cd8d03949566a2c45d828dc5937
Charles Fan <cfan0042@student.monash.edu>
2025-08-10 23:55:57 +1000

    Update index.js for Amazon secret manager

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import spotRoutes from "./routes/spot.js";
-import { db } from "./db.js";
+import { getDB } from "./db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(cors());
+app.use(cors({origin: "http://3.27.186.53:8080"}));
 app.use(express.json());
 // Root welcome message
 app.get("/", (req, res) => {
@@ -21,6 +21,7 @@ app.get("/health", (_req, res) => {
 // Test DB connection
 app.get("/api/testdb", async (req, res) => {
     try {
+        const pool = await getDB();
         const [rows] = await db.query("SELECT 1");
         res.json({
             message: "✅ Database connected successfully!",
@@ -34,7 +35,7 @@ app.get("/api/testdb", async (req, res) => {
 });
 // Parking spots route
 app.use("/api/spot", spotRoutes);
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 //# sourceMappingURL=index.js.map
