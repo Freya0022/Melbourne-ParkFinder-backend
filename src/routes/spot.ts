f41a2aeff5f1c486d20f7f52565d60745e788235
flin0022 <fulilin0022@gmail.com>
2025-08-12 14:53:26 +1000

    for local dev

--- src/routes/spot.ts
+++ src/routes/spot.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getDb } from "../db.js";
+import { db } from "../db.js";
 import type { RowDataPacket } from "mysql2";
 
 interface Spot extends RowDataPacket {
@@ -10,13 +10,11 @@ interface Spot extends RowDataPacket {
   restriction: string;
   restric_hour: number;
 }
-
 const router = Router();
 
 router.get("/", async (req, res) => {
   try {
-    const pool = await getDb();
-    const [rows] = await pool.query<Spot[]>(`
+    const [rows] = await db.query<Spot[]>(`
       SELECT  
       ROW_NUMBER() OVER () AS id, 
       ParkingZone AS parkingZone, 
@@ -26,8 +24,7 @@ router.get("/", async (req, res) => {
       CAST(REGEXP_SUBSTR(restriction_display, '[0-9]+') AS UNSIGNED) AS hour
       FROM bay_segments
     `);
-
-    res.json(rows);
+ res.json(rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Error fetching parking spots" });
